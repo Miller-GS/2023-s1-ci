70caf45e390a1405ddff918406767868392fef95
Gustavo Miller Santos <gumiller2003@gmail.com>
2023-04-13 19:53:54 -0300

    lint: flake8

--- simple_web_app/string_validators.py
+++ simple_web_app/string_validators.py
@@ -74,6 +74,7 @@ class UpperCaseValidator(Validator):
             msg = "Passwords must have at least 1 upper case letter!"
             raise NoUpperCaseException(detail=msg)
 
+
 class ForbiddenCharactersValidator(Validator):
     def __init__(self):
         self.forbidden_set = set("/^~")
