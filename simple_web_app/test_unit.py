2d24b946e8af8444498a57f0595ff3e508f0de85
Gustavo Miller Santos <gumiller2003@gmail.com>
2023-04-13 19:48:27 -0300

    feat: add tests to validate forbidden characters

--- simple_web_app/test_unit.py
+++ simple_web_app/test_unit.py
@@ -67,3 +67,24 @@ def test_8_chars_with_no_upper_case_password_should_raise_exception():
     with pytest.raises(Exception):  # then
         # when
         validator.password_validate()
+
+def test_8_chars_with_forward_slash_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="C@abc@a1/")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
+
+def test_8_chars_with_circumflex_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="C@abc@a1^")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
+
+def test_8_chars_with_tilde_should_raise_exception():
+    # given
+    validator = PasswordValidator(content="C@abc@a1~")
+    with pytest.raises(Exception):  # then
+        # when
+        validator.password_validate()
